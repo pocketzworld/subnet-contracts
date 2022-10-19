6ba9583e1bf90dd4270211c9f98d6cd764dd7c51
Marko Culinovic <marko.culinovic@gmail.com>
2022-10-19 11:23:03 +0200

    Formatting

--- contracts/vault/mainnet/HRVaultProxyAdmin.sol
+++ contracts/vault/mainnet/HRVaultProxyAdmin.sol
@@ -4,7 +4,8 @@ pragma solidity =0.8.12;
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 contract HRVaultProxyAdmin is ProxyAdmin {
-    address public constant INITIAL_OWNER = 0x9b10b6A50bf93E0eec102D7251107880F6192022;
+    address public constant INITIAL_OWNER =
+        0x9b10b6A50bf93E0eec102D7251107880F6192022;
 
     modifier notInitialized() {
         require(owner() == address(0), "Vault Proxy Admin already initialized");
