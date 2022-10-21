c223c30e28937d47d08d9d4f3438cb40f3fe4738
Marko Culinovic <marko.culinovic@gmail.com>
2022-10-21 09:59:06 +0200

    Vault Proxy code style improvements
    [VPY-01C]
    [VPY-02C]
    [VPY-03C]
    [VPY-04C]

--- contracts/vault/mainnet/HRVaultProxy.sol
+++ contracts/vault/mainnet/HRVaultProxy.sol
@@ -8,37 +8,24 @@ contract HRVaultProxy is TransparentUpgradeableProxy {
         0x0300000000000000000000000000000000000000;
     address public constant INITIAL_LOGIC_ADDRESS =
         0x0300000000000000000000000000000000000001;
-    address public constant INITIAL_OWNER =
-        0x9b10b6A50bf93E0eec102D7251107880F6192022;
 
-    constructor(
-        address _logic,
-        address admin_,
-        bytes memory _data
-    ) payable TransparentUpgradeableProxy(_logic, admin_, _data) {}
+    constructor()
+        TransparentUpgradeableProxy(
+            INITIAL_LOGIC_ADDRESS,
+            INITIAL_ADMIN_ADDRESS,
+            ""
+        )
+    {}
 
     modifier notInitialized() {
         require(
-            StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value ==
-                address(0),
+            _getImplementation() == address(0),
             "Implementation already initialized"
         );
-        require(
-            StorageSlot.getAddressSlot(_ADMIN_SLOT).value == address(0),
-            "Proxy admin already initialized"
-        );
         _;
     }
 
-    modifier onlyAdmin() {
-        require(
-            msg.sender == INITIAL_OWNER,
-            "Only owner can initialize proxy admin"
-        );
-        _;
-    }
-
-    function init() public onlyAdmin notInitialized {
+    function init() public notInitialized {
         _changeAdmin(INITIAL_ADMIN_ADDRESS);
         _upgradeTo(INITIAL_LOGIC_ADDRESS);
     }
