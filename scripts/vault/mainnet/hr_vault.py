1061f9e4b3cbb369bbddab591b7dd354a9e3a0ec
Marko Culinovic <marko.culinovic@gmail.com>
2022-10-26 12:11:29 +0200

    Remove c-chain script functions for HR vault

--- scripts/vault/mainnet/hr_vault.py
+++ scripts/vault/mainnet/hr_vault.py
@@ -3,86 +3,12 @@ from typing import Optional
 from brownie import Contract, HRVault, HRVaultProxy, HRVaultProxyAdmin
 from eth_account import Account
 
-from ...common import encode_function_data, get_account, get_vault_admin_account
+from ...common import get_vault_admin_account
 
 # Subnet contract addresses defined in genesis
 VAULT_PROXY_ADMIN_ADDR = "0x0300000000000000000000000000000000000000"
 VAULT_IMPL_ADDR = "0x0300000000000000000000000000000000000001"
 VAULT_PROXY_ADDR = "0x0100000000000000000000000000000000000000"
-
-# ------------------ DEPLOYMENT HELPERS FOR C-CHAIN TESTING -----------------
-
-
-def deploy_vault_proxy(
-    vault_proxy_admin_address: str,
-    vault_impl_address: str,
-    account: Optional[Account] = None,
-):
-    if not account:
-        account = get_account()
-    vault = Contract.from_abi("HRVault", vault_impl_address, HRVault.abi)
-    vault_encoded_initializer_function = encode_function_data(
-        vault.initialize,
-    )
-    vault_proxy = HRVaultProxy.deploy(
-        vault_impl_address,
-        vault_proxy_admin_address,
-        vault_encoded_initializer_function,
-        {"from": account},
-    )
-    vault_proxy_address = vault_proxy.address
-    print(f"Vault proxy deployed at: {vault_proxy_address}")
-
-
-def deploy_vault_proxy_admin(account: Optional[Account] = None) -> str:
-    if not account:
-        account = get_account()
-    # Deploy proxy admin
-    proxy_admin = HRVaultProxyAdmin.deploy({"from": account})
-    proxy_admin_address = proxy_admin.address
-    print(f"Vault Proxy Admin deployed at: {proxy_admin_address}")
-    return proxy_admin_address
-
-
-def deploy_vault_implementation(account: Optional[Account] = None) -> str:
-    if not account:
-        account = get_account()
-    vault_implementation = HRVault.deploy({"from": account})
-    impl_address = vault_implementation.address
-    print(f"Vault implementation deployed at: {impl_address}")
-    return impl_address
-
-
-def verify_vault_proxy_admin(proxy_admin_address: str):
-    contract = HRVaultProxyAdmin.at(proxy_admin_address)
-    HRVaultProxyAdmin.publish_source(contract)
-
-
-def verify_vault_proxy(proxy_address: str):
-    contract = HRVaultProxy.at(proxy_address)
-    HRVaultProxy.publish_source(contract)
-
-
-def verify_vault(vault_address: str):
-    contract = HRVault.at(vault_address)
-    HRVault.publish_source(contract)
-
-
-def deploy():
-    account = get_account()
-    impl_address = deploy_vault_implementation(account)
-    proxy_admin_address = deploy_vault_proxy_admin(account)
-    deploy_vault_proxy(proxy_admin_address, impl_address, account)
-
-
-def verify(proxy_admin_address: str, vault_address: str, vault_proxy_address: str):
-    verify_vault(vault_address)
-    verify_vault_proxy(vault_proxy_address)
-    verify_vault_proxy_admin(proxy_admin_address)
-
-
-# ---------------------------------------------------------------------------
-
 
 # -------------- INIT FUNCTIONS FOR SUBNET ----------------------------------
 
