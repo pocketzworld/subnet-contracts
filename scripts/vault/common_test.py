798d5e2156c950b5118f71405c25107ad99639ad
Marko Culinovic <marko.culinovic@gmail.com>
2022-10-27 21:05:24 +0200

    Cleanup test scripts

--- scripts/vault/common_test.py
+++ scripts/vault/common_test.py
@@ -1,82 +1,17 @@
 import os
 
-from brownie import Contract, HRVault, HRVaultProxy, HRVaultProxyAdmin, accounts
+from brownie import Contract
 from brownie.network.account import Account
 
 from scripts.precompiles.allow_list import Precompile, fetch_interface
 
-from ...common import get_vault_admin_account
-from .hr_vault import VAULT_IMPL_ADDR, VAULT_PROXY_ADDR, VAULT_PROXY_ADMIN_ADDR
+from . import VAULT_IMPL_ADDR, VAULT_PROXY_ADMIN_ADDR
 
 
-def test_vault_initialized(account: Account):
-    vault = Contract.from_abi("HRVault", VAULT_IMPL_ADDR, HRVault.abi)
-    try:
-        vault.initialize({"from": account}).wait(1)
-    except Exception as e:
-        if "Initializable: contract is already initialized" in str(e):
-            print("SUCCESS - Vault already initialized")
-        else:
-            print("FAILURE - Vault intialization passed")
-            raise e
-
-    if vault.owner() == account.address:
-        print(f"SUCCESS - Vault owner is {account.address}")
-    else:
-        msg = f"FAILURE - Vault owner is {account.address}"
-        raise Exception(msg)
-
-
-def test_vault_proxy_initialized(account: Account):
-    vault = Contract.from_abi("HRVault", VAULT_PROXY_ADDR, HRVault.abi)
-    try:
-        vault.initialize({"from": account}).wait(1)
-    except Exception as e:
-        if "Initializable: contract is already initialized" in str(e):
-            print("SUCCESS - Vault proxy already initialized")
-        else:
-            print("FAILURE - Vault proxy intialization passed")
-            raise e
-
-    if vault.owner() == account.address:
-        print(f"SUCCESS - Vault owner is {account.address}")
-    else:
-        msg = f"FAILURE - Vault owner is {account.address}"
-        raise Exception(msg)
-
-    if vault.balance() > 0:
-        print(f"SUCCESS - Vault has balance > 0")
-    else:
-        raise Exception(f"FAILURE - Vault balnce is {vault.balnce()}")
-
-
-def test_vault_impl_cannot_receive(account: Account):
-    vault = Contract.from_abi("HRVault", VAULT_IMPL_ADDR, HRVault.abi)
-    try:
-        account.transfer(vault.address, 1)
-    except Exception as e:
-        if "execution reverted'. This transaction will likely revert" in str(e):
-            print("SUCESS - Vault implementation cannot receive native transfers")
-        else:
-            print("FAILURE - Vault implementation cannot receive native transfers")
-            raise e
-
-
-def test_vault_proxy_cannot_receive(account: Account):
-    vault = Contract.from_abi("HRVault", VAULT_PROXY_ADDR, HRVault.abi)
-    try:
-        account.transfer(vault.address, 1)
-    except Exception as e:
-        if "execution reverted'. This transaction will likely revert" in str(e):
-            print("SUCESS - Vault proxy cannot receive native transfers")
-        else:
-            print("FAILURE - Vault proxy cannot receive native transfers")
-            raise e
-
-
-def test_vault_proxy_admin(account: Account):
-    vault_proxy = HRVaultProxy.at(VAULT_PROXY_ADDR)
-    vault_proxy_admin = HRVaultProxyAdmin.at(VAULT_PROXY_ADMIN_ADDR)
+def test_vault_proxy_admin(
+    account: Account, vault_proxy: Contract, vault_proxy_admin: Contract
+):
+    print("-------------------- TEST VAULT PROXY ADMIN -------------------")
     if vault_proxy_admin.owner() == account.address:
         print(f"SUCCESS - Vault proxy admin owner is {account.address}")
     else:
@@ -94,13 +29,89 @@ def test_vault_proxy_admin(account: Account):
     else:
         msg = f"FAILURE - Vault proxy implementation at {vault_proxy_admin.getProxyImplementation(vault_proxy.address)}"
         raise Exception(msg)
+    print("---------------------------------------------------------------")
 
 
-def test_ownership_transfers(account: Account):
-    vault_proxy_admin = HRVaultProxyAdmin.at(VAULT_PROXY_ADMIN_ADDR)
-    vault_impl = HRVault.at(VAULT_IMPL_ADDR)
-    vault_proxy = Contract.from_abi("HRVault", VAULT_PROXY_ADDR, HRVault.abi)
+def test_vault_initialized(account: Account, vault: Contract):
+    print("-------------------- TEST VAULT INITIALIZED -------------------")
+    try:
+        vault.initialize({"from": account}).wait(1)
+    except Exception as e:
+        if "Initializable: contract is already initialized" in str(e):
+            print("SUCCESS - Vault already initialized")
+        else:
+            print("FAILURE - Vault intialization passed")
+            raise e
+    else:
+        raise Exception("FAILURE - Vault intialization passed")
 
+    if vault.owner() == account.address:
+        print(f"SUCCESS - Vault owner is {account.address}")
+    else:
+        msg = f"FAILURE - Vault owner is {account.address}"
+        raise Exception(msg)
+    print("---------------------------------------------------------------")
+
+
+def test_vault_proxy_initialized(account: Account, vault: Contract):
+    print("-------------------- TEST VAULT PROXY INITIALIZED -------------------")
+    try:
+        vault.initialize({"from": account}).wait(1)
+    except Exception as e:
+        if "Initializable: contract is already initialized" in str(e):
+            print("SUCCESS - Vault proxy already initialized")
+        else:
+            print("FAILURE - Vault proxy intialization passed")
+            raise e
+    else:
+        raise Exception("FAILURE - Vault proxy intialization passed")
+
+    if vault.owner() == account.address:
+        print(f"SUCCESS - Vault owner is {account.address}")
+    else:
+        msg = f"FAILURE - Vault owner is {account.address}"
+        raise Exception(msg)
+
+    if vault.balance() > 0:
+        print(f"SUCCESS - Vault has balance > 0")
+    else:
+        raise Exception(f"FAILURE - Vault balnce is {vault.balnce()}")
+    print("---------------------------------------------------------------")
+
+
+def test_vault_cannot_receive(account: Account, vault: Contract):
+    print("-------------------- TEST VAULT CANNOT RECEIVE -------------------")
+    try:
+        account.transfer(vault.address, 1).wait(1)
+    except Exception as e:
+        if "execution reverted'. This transaction will likely revert" in str(e):
+            print(f"SUCCESS - {vault.address} cannot receive native transfers")
+        else:
+            raise e
+    else:
+        msg = f"FAILURE - {vault.address} can receive native transfers"
+        raise Exception(msg)
+    print("---------------------------------------------------------------")
+
+
+def test_vault_can_receive(account: Account, vault: Contract):
+    print("-------------------- TEST VAULT CAN RECEIVE -------------------")
+    try:
+        account.transfer(vault.address, 1).wait(1)
+    except Exception as e:
+        print(f"FAILURE - {vault.address} cannot receive native transfers")
+        raise e
+    print(f"SUCCESS - {vault.address} can receive native transfers")
+    print("---------------------------------------------------------------")
+
+
+def test_ownership_transfers(
+    account: Account,
+    vault_proxy_admin: Contract,
+    vault_impl: Contract,
+    vault_proxy: Contract,
+):
+    print("-------------------- TEST OWNERSHIP TRANSFERS -------------------")
     try:
         vault_impl.transferOwnership(account.address, {"from": account})
     except Exception as e:
@@ -108,6 +119,9 @@ def test_ownership_transfers(account: Account):
             print("SUCCESS - Vault ownership not transferrable")
         else:
             raise e
+    else:
+        raise Exception("FAILURE - Vault ownership transferrable")
+
     try:
         vault_proxy.transferOwnership(account.address, {"from": account})
     except Exception as e:
@@ -115,6 +129,8 @@ def test_ownership_transfers(account: Account):
             print("SUCCESS - Vault proxy ownership not transferrable")
         else:
             raise e
+    else:
+        raise Exception("FAILURE - Vault proxy ownership transferrable")
 
     try:
         vault_proxy_admin.transferOwnership(account.address, {"from": account})
@@ -123,10 +139,15 @@ def test_ownership_transfers(account: Account):
             print("SUCCESS - Vault proxy admin ownership not transferrable")
         else:
             raise e
+    else:
+        raise Exception("FAILURE - Vault proxy admin ownership transferrable")
+    print("---------------------------------------------------------------")
 
 
-def test_vault_withdrawal(admin_account: Account, account: Account):
-    vault_proxy = Contract.from_abi("HRVault", VAULT_PROXY_ADDR, HRVault.abi)
+def test_vault_withdrawal(
+    admin_account: Account, account: Account, vault_proxy: Contract
+):
+    print("-------------------- TEST VAULT WITHDRAWAL -------------------")
     try:
         vault_proxy.withdraw({"from": account})
     except Exception as e:
@@ -148,9 +169,11 @@ def test_vault_withdrawal(admin_account: Account, account: Account):
         print("FAILURE - vault withdrawal failed")
     else:
         print(f"SUCCESS - vault withdrawal. Current balance {vault_balance_after}")
+    print("---------------------------------------------------------------")
 
 
 def test_allow_list(account: Account, to_enable: Account, precompile: Precompile):
+    print(f"-------------------- TEST {precompile} ALLOW LIST -------------------")
     native_minter = fetch_interface(precompile)
 
     if native_minter.readAllowList(account.address) == 2:
@@ -168,7 +191,7 @@ def test_allow_list(account: Account, to_enable: Account, precompile: Precompile
         native_minter.setEnabled(to_enable, {"from": to_enable}).wait(1)
     except Exception as e:
         if "non-admin cannot modify allow list" in str(e):
-            print("SUCESS - non admin cannot modify allow list")
+            print("SUCCESS - non admin cannot modify allow list")
         else:
             raise e
 
@@ -177,21 +200,4 @@ def test_allow_list(account: Account, to_enable: Account, precompile: Precompile
         print(f"SUCCESS - {to_enable.address} is not {precompile} enabled")
     else:
         raise Exception(f"FAILURE - {to_enable.address} is {precompile} enabled")
-
-
-def test():
-    vault_admin_account = get_vault_admin_account()
-    non_admin_account = accounts.load(os.getenv("TESTNET_ACCOUNT_NAME"))
-    dev_account = accounts.load(os.getenv("DEV_ACCOUNT_NAME"))
-    test_vault_proxy_admin(vault_admin_account)
-    test_vault_initialized(vault_admin_account)
-    test_vault_proxy_initialized(vault_admin_account)
-    test_vault_impl_cannot_receive(vault_admin_account)
-    test_vault_proxy_cannot_receive(vault_admin_account)
-    test_ownership_transfers(non_admin_account)
-    test_vault_withdrawal(vault_admin_account, non_admin_account)
-    test_allow_list(vault_admin_account, dev_account, "native_minter")
-    test_allow_list(vault_admin_account, dev_account, "deployer_list")
-    test_allow_list(vault_admin_account, dev_account, "fee_config_manager")
-    print("------------------------------------------------------------")
-    print("SUBNET SETUP IS VALID")
+    print("---------------------------------------------------------------")
