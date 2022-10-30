5d8b40a3770349b1b70ddaa66e263fb5c8fd026c
Marko Culinovic <marko.culinovic@gmail.com>
2022-10-30 11:34:21 +0100

    Tweak dev setup script

--- scripts/devnet/dev.py
+++ scripts/devnet/dev.py
@@ -7,18 +7,26 @@ CONST_ADDRESS_DEPLOYER_ACCOUNT = "0x6F2D0eB998F77595621DB2805872A894F0F1DEfB"
 GATEWAY_DEPLOYER_ACCOUNT = "0xBa86A5719722B02a5D5e388999C25f3333c7A9fb"
 
 
-def init_dev_subnet():
+def fund_vault_admin():
     account = get_account()
     vault_admin_account = get_vault_admin_account()
-    # Fund accounts
+    # Fund vault admin account
     fund_account(vault_admin_account.address, 1, account)
+
+
+def init_dev_subnet():
+    account = get_account()
+    # Init vault
+    init_subnet()
+    print_subnet_setup(1)
+    # Update gas limit for bridge to work
+    set_fee_config("DEV", account)
+
+
+def enable_bridge_accounts():
+    account = get_account()
     fund_account(CONST_ADDRESS_DEPLOYER_ACCOUNT, 1, account)
     fund_account(GATEWAY_DEPLOYER_ACCOUNT, 1, account)
     # Allow deployments for bridge
     set_enabled("deployer_list", CONST_ADDRESS_DEPLOYER_ACCOUNT, account)
     set_enabled("deployer_list", GATEWAY_DEPLOYER_ACCOUNT, account)
-    # Init vault
-    init_subnet()
-    print_subnet_setup(1)
-    # Update gas limit for bridge
-    set_fee_config("DEV", account)
